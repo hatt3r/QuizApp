fa89d6b0e72327c2c8d8d7a7806ae1a4f960e17a
Serene <serene719@gmail.com>
2023-11-23 12:50:28 +0530

    adding activities and data class for questions

--- app/src/main/java/com/cessabit/quizapp/Constants.kt
+++ app/src/main/java/com/cessabit/quizapp/Constants.kt
@@ -6,9 +6,20 @@ object Constants {
 
         val que1 = Question(
             1,"What country does this flag belong to ?",R.drawable.ic_flag_of_australia,"Australia","Argentina","New Zealand","Armenia",1
-
         )
         questionsList.add(que1)
+        val que2 = Question(
+            1,"What country does this flag belong to ?",R.drawable.ic_flag_of_argentina,"India","Argentina","New Zealand","Romania",2
+        )
+        questionsList.add(que2)
+        val que3 = Question(
+            1,"What country does this flag belong to ?",R.drawable.ic_flag_of_belgium,"India","Argentina","Belgium","Australia",3
+        )
+        questionsList.add(que3)
+        val que4 = Question(
+            1,"What country does this flag belong to ?",R.drawable.ic_flag_of_brazil,"Brazil","Australia","Belgium","India",1
+        )
+        questionsList.add(que4)
         return questionsList
     }
 }
